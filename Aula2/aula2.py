d3459ec258e3c3f3abe4b39a614f265398673d1d
aninhalusousa <analuusousa01@gmail.com>
2024-12-05 11:26:17 -0300

    Adicionando arquivos aula 2
    Aprendi sobre variáveis.

---
+++ Aula2/aula2.py
@@ -0,0 +1,10 @@
+print('Hello, world')
+nome_usuario = 'Ana Luiza' # tipo -> str = string
+idade_usuario = 18 # tipo -> = integer
+altura_usuario = 1.57 # tipo - > = float = flutuante 
+maior_idade = True #tipo - > = bool = boolean 
+print('Seja Bem-Vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('você tem', altura_usuario, 'metros')
+print('você é maior de idade?', maior_idade)
+
